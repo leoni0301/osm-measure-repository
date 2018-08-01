00ccbf30c8c72bb45a4a397b20240c764d58f01b
Leoni <Moeske@stud.uni-heidelberg.de>
2018-08-01 19:22:53 +0200

    init MeasureNumberOfHeavilyEditedObjects

---
+++ MeasureNumberOfHeavilyEditedObjects/src/main/java/org/giscience/osmMeasures/repository/MeasureNumberOfHeavilyEditedObjects.java
@@ -0,0 +1,44 @@
+package org.giscience.osmMeasures.repository;
+
+import org.giscience.measures.rest.measure.MeasureOSHDB;
+import org.giscience.measures.rest.server.OSHDBRequestParameter;
+import org.giscience.measures.tools.Cast;
+import org.giscience.utils.geogrid.cells.GridCell;
+import org.heigit.bigspatialdata.oshdb.api.mapreducer.MapAggregator;
+import org.heigit.bigspatialdata.oshdb.api.object.OSMEntitySnapshot;
+import org.heigit.bigspatialdata.oshdb.osm.OSMType;
+
+import java.util.SortedMap;
+
+public class MeasureNumberOfHeavilyEditedObjects extends MeasureOSHDB<Number, OSMEntitySnapshot> {
+
+/*
+    @Override
+    public Boolean refersToTimeSpan() {
+        return false;
+    }
+
+    @Override
+    public Integer defaultDaysBefore() {
+        return 3 * 12 * 30;
+    }
+
+    @Override
+    public Integer defaultIntervalInDays() {
+        return 30;
+    }
+*/
+
+    @Override
+    public SortedMap<GridCell, Number> compute(MapAggregator<GridCell, OSMEntitySnapshot> mapReducer, OSHDBRequestParameter p) throws Exception {
+        // EXAMPLE ONLY - PLEASE INSERT CODE HERE
+        return Cast.result(mapReducer
+                .osmType(OSMType.WAY)
+                .osmTag("highway", "motorway")
+                .osmEntityFilter( entity -> entity.getVersion() >= 15)
+                .map(OSMEntitySnapshot::getEntity)
+                .count());
+
+        // EXAMPLE END
+    }
+}
